4e6a697d51c716422edbf0d75984cb274535b438
서재은 <wodms5522@naver.com>
2024-03-07 14:35:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "";
+        Queue<String> c1 = new LinkedList<>();
+        Queue<String> c2 = new LinkedList<>();
+        
+        for(int i = 0; i < cards1.length; i++) {
+            c1.add(cards1[i]);
+        }
+        for(int i = 0; i < cards2.length; i++) {
+            c2.add(cards2[i]);
+        }
+        
+        for(int i = 0; i < goal.length; i++) {
+            if(goal[i].equals(c1.peek())) {
+                c1.remove();
+                answer = "Yes";
+            }
+            else if(goal[i].equals(c2.peek())) {
+                c2.remove();
+                answer = "Yes";
+            }
+            else {
+                return "No";
+            }
+            
+            
+        }
+        return answer;
+    }
+}
