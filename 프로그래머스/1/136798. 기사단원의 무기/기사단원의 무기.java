ad8c1bfb52ebde8b3fb0fbc878ebf8f8f968a4db
서재은 <wodms5522@naver.com>
2024-03-08 17:57:10 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.47 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int[] factors = new int[number];
+        
+        for(int i = 1; i <= number; i++) {
+            int cnt = 0;
+            for(int j = 1; j * j <= i; j++) {
+                if(j * j == i) {
+                    cnt++;
+                }
+                else if(i % j == 0) {
+                    cnt += 2;
+                }
+            }
+            factors[i - 1] = cnt;
+        }
+        for(int k = 0; k < factors.length; k++) {
+            if(factors[k] > limit) { // 제한수치를 초과한 기사
+                factors[k] = power;
+            }
+            answer += factors[k];
+        }
+
+        return answer;
+    }
+}
