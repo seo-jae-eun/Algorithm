372f6e8c87f52068ceb3be112624fcf9bc80e919
서재은 <wodms5522@naver.com>
2024-03-22 00:53:50 +0900

    Update A＋B － 3.java

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] sum = new int[N];
-
-        for(int i = 0; i < N; i++) {
-        	int A = sc.nextInt();
-        	int B = sc.nextInt();
-        	
-        	sum[i] = A + B;
-        }
-        for(int n : sum) {
-        	System.out.println(n);
-        }
+	        Scanner sc = new Scanner(System.in);
+	
+	        int N = sc.nextInt();
+	        int[] sum = new int[N];
+	
+	        for(int i = 0; i < N; i++) {
+	        	int A = sc.nextInt();
+	        	int B = sc.nextInt();
+	        	
+	        	sum[i] = A + B;
+	        }
+	        for(int n : sum) {
+	        	System.out.println(n);
+        	}
 	}
 }
