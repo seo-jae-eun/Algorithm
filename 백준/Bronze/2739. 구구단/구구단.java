5d30fbf4f1bf745a9189841e005b3b18baced09b
서재은 <wodms5522@naver.com>
2024-03-22 00:36:15 +0900

    [Bronze V] Title: 구구단, Time: 232 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        System.out.println(N + " * 1 = " + N * 1);
+        System.out.println(N + " * 2 = " + N * 2);
+        System.out.println(N + " * 3 = " + N * 3);
+        System.out.println(N + " * 4 = " + N * 4);
+        System.out.println(N + " * 5 = " + N * 5);
+        System.out.println(N + " * 6 = " + N * 6);
+        System.out.println(N + " * 7 = " + N * 7);
+        System.out.println(N + " * 8 = " + N * 8);
+        System.out.println(N + " * 9 = " + N * 9);
+	}
+}
