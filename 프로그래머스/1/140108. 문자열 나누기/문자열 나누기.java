0bdfc895bcc1cc6361ee0231844076d2ac7c0e38
서재은 <wodms5522@naver.com>
2024-03-22 19:10:10 +0900

    [level 1] Title: 문자열 나누기, Time: 4.86 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -5,40 +5,34 @@ class Solution {
         int answer = 0;
         Queue<Character> q = new LinkedList<>();
 
-        int cntX = 0;
-        int cntNotX = 0;
+        int cntX = 0; // x와 같은 글자들이 나온 횟수
+        int cntNotX = 0; // x와 다른 글자들이 나온 횟수
  
         for(int i = 0; i < s.length(); i++) { // 큐에 넣기
             q.add(s.charAt(i));
         }
         
-        char c = q.peek();
+        char c = q.peek(); // 첫 글자 x
         
         while(!q.isEmpty()) {
-            System.out.println(c);
-            if(c == q.poll()) cntX++;
-            else cntNotX++;
-            System.out.println(cntX);
-            System.out.println(cntNotX);
+            if(c == q.poll()) cntX++; // x와 같을 때
+            else cntNotX++; // x와 다를 때
             
-            if(cntX == cntNotX) {
+            if(cntX == cntNotX) { // 두 횟수가 같아지는 순간
                 answer++;
                 cntX = 0;
                 cntNotX = 0;
                 if(q.isEmpty()) break;
-
                 c = q.peek();
             }
             else {
-                if(q.size() <= 1 && cntX > cntNotX) {
+                if(q.size() <= 1 && cntX > cntNotX) { // 맨끝 부분
                     answer++; 
                     break;
                 } 
             }
         }
-        
-        
-            
+          
         return answer;
     }
 }
