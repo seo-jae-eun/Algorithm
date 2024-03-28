97500a88e45fc33c54dd9e037b9a3b0e843a4d77
서재은 <wodms5522@naver.com>
2024-03-28 23:09:40 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.10 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -11,27 +11,27 @@ class Solution {
             countX[i] = countChar(X, (char)('0' + i));
             countY[i] = countChar(Y, (char)('0' + i));
         }
+        
         StringBuilder sb = new StringBuilder();
         for(int j = 9; j >= 0; j--) {
             if(countX[j] == 0 || countY[j] == 0) continue;
             
             int number = Math.min(countX[j], countY[j]);
             String s = String.valueOf(j);
-            while(number > 0) {
+            while(number > 0) { // 공통으로 나타나는 가장 큰 정수 만들기
                 sb.append(s);
                 number--;
             }
         }
-        answer = sb.toString();
+        answer = sb.toString();        
         
-        
-        if(answer.equals("")) return "-1";
-        if((answer.split("")[0]).equals("0")) return "0";
+        if(answer.equals("")) return "-1"; // 짝꿍이 존재하지 않을 때
+        if((answer.split("")[0]).equals("0")) return "0"; // 제일 앞자리가 0일때
 
         return answer;
     }
     
-        public static int countChar(String str, char ch) {
+        public static int countChar(String str, char ch) { // 0~9 개수 구하기
             return str.length() - str.replace(String.valueOf(ch), "").length();    
         }
 }
