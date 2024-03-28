039188c00803635e21f40c3c209658861f901021
서재은 <wodms5522@naver.com>
2024-03-28 23:12:06 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.17 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -6,24 +6,23 @@ class Solution {
 
         int[] countX = new int[10];
         int[] countY = new int[10];
+        List<Integer> s = new ArrayList<>();
         
         for(int i = 0; i < countX.length; i++) {
             countX[i] = countChar(X, (char)('0' + i));
             countY[i] = countChar(Y, (char)('0' + i));
         }
-        
-        StringBuilder sb = new StringBuilder();
         for(int j = 9; j >= 0; j--) {
-            if(countX[j] == 0 || countY[j] == 0) continue;
-            
-            int number = Math.min(countX[j], countY[j]);
-            String s = String.valueOf(j);
-            while(number > 0) { // 공통으로 나타나는 가장 큰 정수 만들기
-                sb.append(s);
-                number--;
+            if(countX[j] != 0 && countY[j] != 0) {
+                for(int k = 0; k < Math.min(countX[j], countY[j]); k++) {
+                    s.add(j);
+                }
             }
         }
-        answer = sb.toString();        
+        // Collections.sort(s, Collections.reverseOrder());
+
+        answer = s.toString().replaceAll("[^0-9]","");
+        
         
         if(answer.equals("")) return "-1"; // 짝꿍이 존재하지 않을 때
         if((answer.split("")[0]).equals("0")) return "0"; // 제일 앞자리가 0일때
@@ -31,7 +30,7 @@ class Solution {
         return answer;
     }
     
-        public static int countChar(String str, char ch) { // 0~9 개수 구하기
+        public static int countChar(String str, char ch) {
             return str.length() - str.replace(String.valueOf(ch), "").length();    
         }
 }
